fb4f329520bbbe3c6a9bcf28d0521add323c5cc7
mani9kanta3 <151725075+mani9kanta3@users.noreply.github.com>
2026-08-20 16:40:46 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Doctors from "./Doctors";
-import SingleDoctor from "./SingleDoctor";
-import Contact from "./Contact";
+import React from 'react'
+import './App.css'
+import Navbar from './Navbar'
+import HomePage from './HomePage'
+import Footer from './Footer'
+import PatientRegistration from './PatientRegistration'
+import LoginPage from './LoginPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Appointment from './Appointment'
+import Dashboard from './Dashboard'
+import ProtectedRoute from './ProtectedRoute'
+import ForgotPassword from './ForgotPassword'
 
 function App() {
   return (
+    <>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Doctors />} />
-
-        <Route path="/doctors" element={<Doctors />} />
-        <Route path="/doctors/:id" element={<SingleDoctor />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/login' element={<LoginPage/>} />
+      <Route path='/forgot-password' element={<ForgotPassword/>} />
+      <Route path='/appointment' element={<ProtectedRoute><Appointment/></ProtectedRoute>}/>
+      <Route path='/dashboard' element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+    </Routes>
+    <Footer/>
     </BrowserRouter>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
